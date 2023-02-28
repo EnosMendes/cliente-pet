3189083369d659edeb29e77faa8e575b5676a9a1
Enos Mendes <enos981077559@gmail.com>
2023-02-27 23:36:19 -0300

    implementa controller e fluxo para service

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteService.java
@@ -0,0 +1,9 @@
+package br.com.petz.clientepet.cliente.application.service;
+
+import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
+import br.com.petz.clientepet.cliente.application.api.ClienteResquest;
+
+public interface ClienteService {
+	ClienteResponse criaCliente(ClienteResquest clienteRequest);
+
+}
